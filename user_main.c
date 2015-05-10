df6f6ba4b20d6741ff87bed8d7a2c6107686295c
Erik Slagter <erik@slagter.name>
2015-05-10 10:54:58 +0200

    Cosmetics, rename periodic task to receive task.

--- user_main.c
+++ user_main.c
@@ -12,12 +12,12 @@
 
 enum
 {
-	periodic_task_id			= 0,
-	periodic_task_queue_length	= 64,
+	receive_task_id				= 0,
+	receive_task_queue_length	= 64,
 };
 
 static struct espconn	*esp_connection;
-static os_event_t		receive_task_queue[periodic_task_queue_length];
+static os_event_t		receive_task_queue[receive_task_queue_length];
 
 ICACHE_FLASH_ATTR static uint8_t uart_rxfifo_error(void)
 {
@@ -73,7 +73,7 @@ ICACHE_FLASH_ATTR static void uart_rx_callback(void *p)
 	if(uart_rxfifo_full() || uart_rxfifo_available())
 	{
 		ETS_UART_INTR_DISABLE();
-		system_os_post(periodic_task_id, 0, 0);
+		system_os_post(receive_task_id, 0, 0);
 	}
 }
 
@@ -128,7 +128,7 @@ ICACHE_FLASH_ATTR static void uart_transmit(uint16_t length, uint8 *buffer)
 	}
 }
 
-ICACHE_FLASH_ATTR static void uart_periodic(os_event_t *events)
+ICACHE_FLASH_ATTR static void uart_receive_task(os_event_t *events)
 {
 	char		buffer[256];
 	uint16_t	length;
@@ -211,5 +211,5 @@ ICACHE_FLASH_ATTR void user_init(void)
 
 	uart_init(460800, EIGHT_BITS, PARITY_DISABLE, ONE_STOP_BIT);
 
-	system_os_task(uart_periodic, periodic_task_id, receive_task_queue, periodic_task_queue_length);
+	system_os_task(uart_receive_task, receive_task_id, receive_task_queue, receive_task_queue_length);
 }
