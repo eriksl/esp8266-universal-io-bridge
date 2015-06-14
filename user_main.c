910065b9dcd9d9a470f2ad7bfa3a5d50d44ce16e
Erik Slagter <erik@slagter.name>
2015-06-14 14:49:37 +0200

    Add base for tcp command port (24, 25 is transparent bridge).

--- user_main.c
+++ user_main.c
@@ -26,6 +26,11 @@ queue_t *uart_receive_queue;
 
 os_event_t background_task_queue[background_task_queue_length];
 
+static char *tcp_cmd_receive_buffer;
+static char *tcp_cmd_send_buffer;
+static char tcp_cmd_send_buffer_busy;
+static struct espconn *esp_cmd_tcp_connection;
+
 static char *tcp_data_send_buffer;
 static char tcp_data_send_buffer_busy;
 static struct espconn *esp_data_tcp_connection;
@@ -36,6 +41,22 @@ ICACHE_FLASH_ATTR static void watchdog_crash(void)
 {
 	for(;;)
 		(void)0;
+}
+
+ICACHE_FLASH_ATTR static void tcp_accept(struct espconn *esp_config, esp_tcp *esp_tcp_config,
+		uint16_t port, void (*connect_callback)(struct espconn *))
+{
+	memset(esp_tcp_config, 0, sizeof(*esp_tcp_config));
+	esp_tcp_config->local_port = port;
+	memset(esp_config, 0, sizeof(*esp_config));
+	esp_config->type = ESPCONN_TCP;
+	esp_config->state = ESPCONN_NONE;
+	esp_config->proto.tcp = esp_tcp_config;
+	espconn_regist_connectcb(esp_config, (espconn_connect_callback)connect_callback);
+	espconn_accept(esp_config);
+	esp_cmd_tcp_connection = 0;
+
+	espconn_tcp_set_max_con_allow(esp_config, 1);
 }
 
 static void background_task(os_event_t *events)
@@ -122,10 +143,8 @@ static void tcp_data_disconnect_callback(void *arg)
 	esp_data_tcp_connection = 0;
 }
 
-static void tcp_data_connect_callback(void *arg)
+static void tcp_data_connect_callback(struct espconn *new_connection)
 {
-	struct espconn *new_connection = (struct espconn *)arg;
-
 	if(esp_data_tcp_connection)
 		espconn_disconnect(new_connection);
 	else
@@ -133,14 +152,106 @@ static void tcp_data_connect_callback(void *arg)
 		esp_data_tcp_connection	= new_connection;
 		tcp_data_send_buffer_busy = 0;
 
-		espconn_regist_recvcb(new_connection, tcp_data_receive_callback);
-		espconn_regist_sentcb(new_connection, tcp_data_sent_callback);
-		espconn_regist_disconcb(new_connection, tcp_data_disconnect_callback);
+		espconn_regist_recvcb(esp_data_tcp_connection, tcp_data_receive_callback);
+		espconn_regist_sentcb(esp_data_tcp_connection, tcp_data_sent_callback);
+		espconn_regist_disconcb(esp_data_tcp_connection, tcp_data_disconnect_callback);
 
-		espconn_set_opt(new_connection, ESPCONN_REUSEADDR);
+		espconn_set_opt(esp_data_tcp_connection, ESPCONN_REUSEADDR);
 
 		queue_flush(uart_send_queue);
 		queue_flush(uart_receive_queue);
+	}
+}
+
+static void tcp_cmd_sent_callback(void *arg)
+{
+    tcp_cmd_send_buffer_busy = 0;
+}
+
+static void tcp_cmd_receive_callback(void *arg, char *data, uint16_t length)
+{
+	uint16_t current;
+	uint16_t tcp_cmd_receive_buffer_length;
+	uint16_t tcp_cmd_send_buffer_length;
+	uint8_t byte;
+	uint8_t telnet_strip_state;
+
+	if(!esp_cmd_tcp_connection)
+		return;
+
+	tcp_cmd_receive_buffer_length = 0;
+
+	telnet_strip_state = ts_raw;
+
+	for(current = 0; (current < length) && (tcp_cmd_receive_buffer_length < buffer_size); current++)
+	{
+		byte = (uint8_t)data[current];
+
+		switch(telnet_strip_state)
+		{
+			case(ts_raw):
+			{
+				if(flags.strip_telnet && (byte == 0xff))
+					telnet_strip_state = ts_dodont;
+				else
+					if((byte >= ' ') && (byte <= '~'))
+						tcp_cmd_receive_buffer[tcp_cmd_receive_buffer_length++] = (char)byte;
+
+				break;
+			}
+
+			case(ts_dodont):
+			{
+				telnet_strip_state = ts_data;
+				break;
+			}
+
+			case(ts_data):
+			{
+				telnet_strip_state = ts_raw;
+				break;
+			}
+		}
+	}
+
+	memcpy(tcp_cmd_send_buffer, tcp_cmd_receive_buffer, tcp_cmd_receive_buffer_length);
+
+	if((tcp_cmd_receive_buffer_length + 2) > buffer_size)
+		tcp_cmd_send_buffer_length = tcp_cmd_receive_buffer_length - 2;
+	else
+		tcp_cmd_send_buffer_length = tcp_cmd_receive_buffer_length;
+
+	tcp_cmd_send_buffer[tcp_cmd_send_buffer_length + 0] = '\r';
+	tcp_cmd_send_buffer[tcp_cmd_send_buffer_length + 1] = '\n';
+
+	tcp_cmd_send_buffer_length += 2;
+
+	if(!tcp_cmd_send_buffer_busy)
+	{
+		tcp_cmd_send_buffer_busy = 1;
+		espconn_sent(esp_cmd_tcp_connection, tcp_cmd_send_buffer, tcp_cmd_send_buffer_length);
+	}
+}
+
+static void tcp_cmd_disconnect_callback(void *arg)
+{
+	esp_cmd_tcp_connection = 0;
+}
+
+static void tcp_cmd_connect_callback(struct espconn *new_connection)
+{
+	if(esp_cmd_tcp_connection)
+		espconn_disconnect(new_connection);
+	else
+	{
+		esp_cmd_tcp_connection = new_connection;
+		tcp_cmd_send_buffer_busy = 0;
+
+		espconn_regist_recvcb(esp_cmd_tcp_connection, tcp_cmd_receive_callback);
+		espconn_regist_sentcb(esp_cmd_tcp_connection, tcp_cmd_sent_callback);
+		espconn_regist_disconcb(esp_cmd_tcp_connection, tcp_cmd_disconnect_callback);
+
+		espconn_set_opt(esp_cmd_tcp_connection, ESPCONN_REUSEADDR);
 	}
 }
 
@@ -154,6 +265,12 @@ ICACHE_FLASH_ATTR void user_init(void)
 	if(!(uart_receive_queue = queue_new(buffer_size)))
 		watchdog_crash();
 
+	if(!(tcp_cmd_receive_buffer = os_malloc(buffer_size)))
+		watchdog_crash();
+
+	if(!(tcp_cmd_send_buffer = os_malloc(buffer_size)))
+		watchdog_crash();
+
 	if(!(tcp_data_send_buffer = os_malloc(buffer_size)))
 		watchdog_crash();
 
@@ -165,8 +282,8 @@ ICACHE_FLASH_ATTR static void user_init2(void)
 	// create ap_auth.h and #define ap_ssid / ap_password accordingly
 
 	static struct station_config station_config = { ap_ssid, ap_password, 0, { 0, 0, 0, 0, 0, 0 } };
-	static struct espconn esp_data_config;
-	static esp_tcp esp_data_tcp_config;
+	static struct espconn esp_cmd_config, esp_data_config;
+	static esp_tcp esp_cmd_tcp_config, esp_data_tcp_config;
 
 	wifi_set_sleep_type(NONE_SLEEP_T);
 	wifi_set_opmode_current(STATION_MODE);
@@ -175,21 +292,13 @@ ICACHE_FLASH_ATTR static void user_init2(void)
 	wifi_station_set_config_current(&station_config);
 	wifi_station_connect();
 
-	espconn_tcp_set_max_con(1);
+	tcp_accept(&esp_cmd_config, &esp_cmd_tcp_config, 24, tcp_cmd_connect_callback);
+	espconn_regist_time(&esp_cmd_config, 0, 0);
+	esp_cmd_tcp_connection = 0;
 
-	esp_data_tcp_connection = 0;
-
-	memset(&esp_data_tcp_config, 0, sizeof(esp_data_tcp_config));
-	esp_data_tcp_config.local_port = 23;
-
-	memset(&esp_data_config, 0, sizeof(esp_data_config));
-	esp_data_config.type		= ESPCONN_TCP;
-	esp_data_config.state		= ESPCONN_NONE;
-	esp_data_config.proto.tcp	= &esp_data_tcp_config;
-
-	espconn_regist_connectcb(&esp_data_config, tcp_data_connect_callback);
-	espconn_accept(&esp_data_config);
+	tcp_accept(&esp_data_config, &esp_data_tcp_config, 25, tcp_data_connect_callback);
 	espconn_regist_time(&esp_data_config, 30, 0);
+	esp_data_tcp_connection = 0;
 
 	uart_init();
 
