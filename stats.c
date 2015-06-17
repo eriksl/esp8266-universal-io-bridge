ca48fc75a3028fab731c883bc9366fd3e7d7d111
Erik Slagter <erik@slagter.name>
2015-06-17 18:53:38 +0200

    Add cpu frequency and free ram to stats command.

--- stats.c
+++ stats.c
@@ -2,7 +2,8 @@
 
 #include "util.h"
 
-#include "c_types.h"
+#include <c_types.h>
+#include <user_interface.h>
 
 stats_t stat_uart_rx_interrupts = 0;
 stats_t stat_uart_tx_interrupts = 0;
@@ -10,15 +11,21 @@ stats_t	stat_application_periodic = 0;
 
 static const char *format_string =
 {
+	"cpu frequency: %u\n"
+	"spi flash id: %u\n"
 	"int uart rx: %u\n"
 	"int uart tx: %u\n"
 	"application periodic: %u\n"
+	"heap free: %u\n"
 };
 
 ICACHE_FLASH_ATTR void stats_generate(size_t size, char *dst)
 {
 	snprintf(dst, size, format_string,
+			system_get_cpu_freq(),
+			spi_flash_get_id(),
 			stat_uart_rx_interrupts,
 			stat_uart_tx_interrupts,
-			stat_application_periodic);
+			stat_application_periodic,
+			system_get_free_heap_size());
 }
