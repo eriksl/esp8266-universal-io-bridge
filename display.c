bc666e059052a47750bc32cc7fe6a3930ff0776c
Erik Slagter <erik@slagter.name>
2016-05-01 15:23:38 +0200

    display: only use one display at a time, saves memory.

--- display.c
+++ display.c
@@ -6,14 +6,14 @@
 #include "stats.h"
 #include "config.h"
 
-typedef const struct
+typedef enum
 {
-	int				const size;
-	const char *	const name;
-	const char *	const type;
-	bool_t			(* const init_fn)(void);
-	bool_t			(* const set_fn)(int brightness, const char *tag, const char *text);
-} display_info_t;
+	display_slot_amount = 8,
+	display_slot_tag_size = 32,
+	display_slot_content_size = 64
+} display_slot_enum_t;
+
+assert_size(display_slot_enum_t, 4);
 
 typedef enum
 {
@@ -24,6 +24,29 @@ typedef enum
 } display_id_t;
 
 assert_size(display_id_t, 4);
+
+typedef const struct
+{
+	int				const size;
+	const char *	const name;
+	const char *	const type;
+	bool_t			(* const init_fn)(void);
+	bool_t			(* const set_fn)(int brightness, const char *tag, const char *text);
+} display_info_t;
+
+typedef struct
+{
+	int	detected;
+	int	current_slot;
+	int	brightness;
+} display_data_t;
+
+typedef struct
+{
+	int		timeout;
+	char	tag[display_slot_tag_size];
+	char	content[display_slot_content_size];
+} display_slot_t;
 
 static roflash display_info_t display_info[display_size] =
 {
@@ -39,108 +62,92 @@ static roflash display_info_t display_info[display_size] =
 	}
 };
 
-static display_data_t display_data[display_size];
-
-static char default_message[display_slot_content_size + 1] = "";
+static display_data_t display_data;
+static display_slot_t display_slot[display_slot_amount];
 
 irom static void display_update(bool_t advance)
 {
 	const char *display_text;
-	int display, slot;
+	int slot;
 	display_info_t *display_info_entry;
-	display_data_t *display_data_entry;
 	string_new(static, tag_text, 32);
 	string_new(static, info_text, 64);
 
-	for(display = 0; display < display_size; display++)
-	{
-		display_info_entry = &display_info[display];
-		display_data_entry = &display_data[display];
+	if(display_data.detected < 0)
+		return;
 
-		if(!display_data_entry->detected)
-			continue;
+	display_info_entry = &display_info[display_data.detected];
 
-		for(slot = display_data_entry->current_slot + (advance ? 1 : 0); slot < display_slot_amount; slot++)
-			if(display_data_entry->slot[slot].content[0])
+	for(slot = display_data.current_slot + (advance ? 1 : 0); slot < display_slot_amount; slot++)
+		if(display_slot[slot].content[0])
+			break;
+	if(slot >= display_slot_amount)
+		for(slot = 0; slot < display_slot_amount; slot++)
+			if(display_slot[slot].content[0])
 				break;
 
-		if(slot >= display_slot_amount)
-			for(slot = 0; slot < display_slot_amount; slot++)
-				if(display_data_entry->slot[slot].content[0])
-					break;
+	if(slot >= display_slot_amount)
+		slot = 0;
 
-		if(slot >= display_slot_amount)
-		{
-			display_data_entry->current_slot = 0;
-			continue;
-		}
+	display_data.current_slot = slot;
+	display_text = display_slot[slot].content;
 
-		display_data_entry->current_slot = slot;
-		display_text = display_data_entry->slot[slot].content;
-
-		if(!ets_strcmp(display_text, "%%%%"))
-		{
-			string_clear(&info_text);
-			string_format(&info_text, "%02u.%02u %s %s",
-					rt_hours, rt_mins, display_info_entry->name, display_info_entry->type);
-			display_text = string_to_ptr(&info_text);
-		}
-
-		if(ets_strcmp(display_data_entry->slot[slot].tag, "-"))
-		{
-			string_clear(&tag_text);
-			string_format(&tag_text, "%02u:%02u ", rt_hours, rt_mins);
-			string_cat_ptr(&tag_text, display_data_entry->slot[slot].tag);
-			string_format(&tag_text, " [%u]", slot);
-			display_info_entry->set_fn(display_data_entry->brightness, string_to_ptr(&tag_text), display_text);
-		}
-		else
-			display_info_entry->set_fn(display_data_entry->brightness, (char *)0, display_text);
+	if(!ets_strcmp(display_text, "%%%%"))
+	{
+		string_clear(&info_text);
+		string_format(&info_text, "%02u.%02u %s %s",
+				rt_hours, rt_mins, display_info_entry->name, display_info_entry->type);
+		display_text = string_to_ptr(&info_text);
 	}
+
+	if(ets_strcmp(display_slot[slot].tag, "-"))
+	{
+		string_clear(&tag_text);
+		string_format(&tag_text, "%02u:%02u ", rt_hours, rt_mins);
+		string_cat_ptr(&tag_text, display_slot[slot].tag);
+		string_format(&tag_text, " [%u]", slot);
+		display_info_entry->set_fn(display_data.brightness, string_to_ptr(&tag_text), display_text);
+	}
+	else
+		display_info_entry->set_fn(display_data.brightness, (char *)0, display_text);
 }
 
 irom void display_periodic(void) // call once per second
 {
 	static int current_page = 0;
-	int display, slot, active_slots;
-	display_data_t *display_data_entry;
+	int active_slots, slot;
+
+	if(display_data.detected < 0)
+		return;
 
 	// expiration
 
-	for(display = 0; display < display_size; display++)
+	active_slots = 0;
+
+	for(slot = 0; slot < display_slot_amount; slot++)
 	{
-		display_data_entry = &display_data[display];
-
-		if(!display_data_entry->detected)
-			continue;
-
-		active_slots = 0;
-
-		for(slot = 0; slot < display_slot_amount; slot++)
+		if(display_slot[slot].timeout > 0)
 		{
-			if(display_data_entry->slot[slot].timeout > 0)
+			if(--display_slot[slot].timeout <= 0)
 			{
-				if(--display_data_entry->slot[slot].timeout == 0)
-				{
-					display_data_entry->slot[slot].tag[0] = '\0';
-					display_data_entry->slot[slot].content[0] = '\0';
-				}
+				display_slot[slot].tag[0] = '\0';
+				display_slot[slot].content[0] = '\0';
 			}
-
-			if(display_data_entry->slot[slot].content[0])
-				active_slots++;
 		}
 
-		if(active_slots == 0)
-		{
-			display_data_entry->slot[0].timeout = 0;
-			strlcpy(display_data_entry->slot[0].tag, "boot", display_slot_tag_size);
-			strlcpy(display_data_entry->slot[0].content, default_message, display_slot_content_size);
-			current_page = 0;
-		}
+		if(display_slot[slot].content[0])
+			active_slots++;
 	}
 
-	if(++current_page > 4)
+	if(active_slots == 0)
+	{
+		display_slot[0].timeout = 0;
+		strlcpy(display_slot[0].tag, "boot", display_slot_tag_size - 1);
+		strlcpy(display_slot[0].content, config.display_default_msg, display_slot_content_size - 1);
+		current_page = 0;
+	}
+
+	if(++current_page > 4) // 4 seconds for each slot
 	{
 		current_page = 0;
 		display_update(true);
@@ -152,138 +159,104 @@ irom void display_periodic(void) // call once per second
 irom void display_init(void)
 {
 	display_info_t *display_info_entry;
-	display_data_t *display_data_entry;
 	int current, slot;
 
-	strlcpy(default_message, config.display_default_msg, sizeof(default_message));
+	display_data.detected = -1;
 
 	for(current = 0; current < display_size; current++)
 	{
 		display_info_entry = &display_info[current];
-		display_data_entry = &display_data[current];
 
 		if(display_info_entry->init_fn && (display_info_entry->init_fn()))
-			display_data_entry->detected = 1;
-		else
-			display_data_entry->detected = 0;
-
-		for(slot = 0; slot < display_slot_amount; slot++)
 		{
-			display_data_entry->current_slot = 0;
-			display_data_entry->brightness = 1;
-			display_data_entry->slot[slot].timeout = 0;
-			display_data_entry->slot[slot].tag[0] = '\0';
-			display_data_entry->slot[slot].content[0] = '\0';
+			display_data.detected = current;
+			break;
 		}
 	}
 
-	display_update(false);
-}
+	display_data.current_slot = 0;
+	display_data.brightness = 1;
 
-irom static bool_t display_set_brightness(display_id_t display, int brightness)
-{
-	display_data_t *display_data_entry;
-
-	if(brightness > 4)
-		return(false);
-
-	if(display >= display_size)
-		return(false);
-
-	display_data_entry = &display_data[display];
-
-	if(!display_data_entry->detected)
-		return(false);
-
-	display_data_entry->brightness = brightness;
-	display_update(false);
-
-	return(true);
-}
-
-irom static bool_t display_get_brightness(display_id_t display, int *brightness)
-{
-	display_data_t *display_data_entry;
-
-	if(display >= display_size)
-		return(false);
-
-	display_data_entry = &display_data[display];
-
-	if(!display_data_entry->detected)
-		return(false);
-
-	*brightness = display_data_entry->brightness;
-
-	return(true);
-}
-
-irom static void display_dump(string_t *dst, int verbose_level)
-{
-	int display;
-	int slot;
-	display_info_t *display_info_entry;
-	display_data_t *display_data_entry;
-	display_slot_t *slot_entry;
-	int original_length = string_length(dst);
-
-	for(display = 0; display < display_size; display++)
+	for(slot = 0; slot < display_slot_amount; slot++)
 	{
-		display_info_entry = &display_info[display];
-		display_data_entry = &display_data[display];
-
-		if((verbose_level > 0) || display_data_entry->detected)
-		{
-			string_format(dst, "> %c display #%u (%s: %s)\n",
-					display_data_entry->detected ? '+' : ' ', display, display_info_entry->name, display_info_entry->type);
-
-			if(verbose_level > 1)
-			{
-				for(slot = 0; slot < display_slot_amount; slot++)
-				{
-					slot_entry = &display_data_entry->slot[slot];
-					string_format(dst, ">> slot %u: timeout %u, tag: \"%s\", text: \"%s\"\n", slot,
-							slot_entry->timeout, slot_entry->tag, slot_entry->content);
-				}
-			}
-		}
+		display_slot[slot].timeout = 0;
+		display_slot[slot].tag[0] = '\0';
+		display_slot[slot].content[0] = '\0';
 	}
 
-	if(original_length == string_length(dst))
-		string_cat(dst, "> no displays found\n");
+	if(display_data.detected >= 0)
+		display_update(false);
+}
+
+irom static bool_t display_set_brightness(int brightness)
+{
+	if(display_data.detected < 0)
+		return(false);
+
+	if((brightness < 0) || (brightness > 4))
+		return(false);
+
+	display_data.brightness = brightness;
+	display_update(false);
+
+	return(true);
+}
+
+irom static bool_t display_get_brightness(int *brightness)
+{
+	if(display_data.detected < 0)
+		return(false);
+
+	*brightness = display_data.brightness;
+
+	return(true);
+}
+
+irom static void display_dump(string_t *dst)
+{
+	display_info_t *display_info_entry;
+	int slot;
+
+	if(display_data.detected < 0)
+	{
+		string_cat(dst, "> no displays detected\n");
+		return;
+	}
+
+	display_info_entry = &display_info[display_data.detected];
+
+	string_format(dst, "> display type #%u (%s: %s)\n", display_data.detected,
+			display_info_entry->name, display_info_entry->type);
+
+	for(slot = 0; slot < display_slot_amount; slot++)
+		string_format(dst, ">> slot %u: timeout %u, tag: \"%s\", text: \"%s\"\n", slot,
+				display_slot[slot].timeout, display_slot[slot].tag, display_slot[slot].content);
 }
 
 irom app_action_t application_function_display_brightness(const string_t *src, string_t *dst)
 {
-	int id, value;
+	int value;
 
-	if((parse_int(1, src, &id, 0) != parse_ok) ||
-			(parse_int(2, src, &value, 0) != parse_ok) ||
-			(value < 0) || (value > 4))
+	if(parse_int(1, src, &value, 0) != parse_ok)
 	{
-		string_cat(dst, "display-brightness: usage: display_id <brightess>=0,1,2,3,4\n");
+		string_cat(dst, "display-brightness: usage: <brightess>=0,1,2,3,4\n");
 		return(app_action_error);
 	}
 
-	if(!display_set_brightness(id, value) || !display_get_brightness(id, &value))
+	if(!display_set_brightness(value) || !display_get_brightness(&value))
 	{
-		string_format(dst, "display-brightness: invalid display: %d\n", id);
+		string_format(dst, "display-brightness: no display or invalid brightness value: %d\n", value);
 		return(app_action_error);
 	}
 
-	string_format(dst, "display %u brightness: %u\n", id, value);
+	string_format(dst, "display brightness: %u\n", value);
 
 	return(app_action_normal);
 }
 
 irom app_action_t application_function_display_dump(const string_t *src, string_t *dst)
 {
-	int verbose;
-
-	if(parse_int(1, src, &verbose, 0) != parse_ok)
-		verbose = 0;
-
-	display_dump(dst, verbose);
+	display_dump(dst);
 
 	return(app_action_normal);
 }
@@ -304,7 +277,7 @@ irom app_action_t application_function_display_default_message(const string_t *s
 			ws--;
 	}
 
-	strlcpy(config.display_default_msg, text, sizeof(config.display_default_msg));
+	strlcpy(config.display_default_msg, text, sizeof(config.display_default_msg) - 1);
 	string_format(dst, "set default display message to \"%s\"\n", config.display_default_msg);
 
 	return(app_action_normal);
@@ -312,24 +285,27 @@ irom app_action_t application_function_display_default_message(const string_t *s
 
 irom app_action_t application_function_display_set(const string_t *src, string_t *dst)
 {
-	display_info_t *display_info_entry;
-	display_data_t *display_data_entry;
-	int display, slot, timeout, current;
+	int slot, timeout, current;
 	const char *text;
 
-	if((parse_int(1, src, &display, 0) != parse_ok) ||
-		(parse_int(2, src, &slot, 0) != parse_ok) ||
-		(parse_int(3, src, &timeout, 0) != parse_ok) ||
-		(parse_string(4, src, dst) != parse_ok))
+	if(display_data.detected < 0)
+	{
+		string_cat(dst, "display_set: no display detected\n");
+		return(app_action_error);
+	}
+
+	if((parse_int(1, src, &slot, 0) != parse_ok) ||
+		(parse_int(2, src, &timeout, 0) != parse_ok) ||
+		(parse_string(3, src, dst) != parse_ok))
 	{
 		string_clear(dst);
-		string_cat(dst, "display-set: usage: display_id slot timeout tag text\n");
+		string_cat(dst, "display-set: usage: slot timeout tag text\n");
 		return(app_action_error);
 	}
 
 	text = src->buffer;
 
-	for(current = 5; current > 0; text++)
+	for(current = 4; current > 0; text++)
 	{
 		if(*text == '\0')
 			break;
@@ -341,45 +317,27 @@ irom app_action_t application_function_display_set(const string_t *src, string_t
 	if(current > 0)
 	{
 		string_clear(dst);
-		string_cat(dst, "display-set: usage: display_id slot timeout tag text\n");
-		return(app_action_error);
-	}
-
-	if(display >= display_size)
-	{
-		string_clear(dst);
-		string_format(dst, "display setslot: display #%d unknown\n", display);
-		return(app_action_error);
-	}
-
-	display_info_entry = &display_info[display];
-	display_data_entry = &display_data[display];
-
-	if(!display_data_entry->detected)
-	{
-		string_clear(dst);
-		string_format(dst, "display setslot: display #%d not found\n", display);
+		string_cat(dst, "display-set: usage: slot timeout tag TEXT\n");
 		return(app_action_error);
 	}
 
 	if(slot > display_slot_amount)
 	{
 		string_clear(dst);
-		string_format(dst, "display setslot: slot #%d out of limits\n", slot);
+		string_format(dst, "display-set: slot #%d out of limits\n", slot);
 		return(app_action_error);
 	}
 
-	strlcpy(display_data_entry->slot[slot].tag, string_to_ptr(dst), display_slot_content_size);
-	strlcpy(display_data_entry->slot[slot].content, text, display_slot_content_size);
-	display_data_entry->slot[slot].timeout = timeout;
+	strlcpy(display_slot[slot].tag, string_to_ptr(dst), display_slot_tag_size - 1);
+	strlcpy(display_slot[slot].content, text, display_slot_content_size - 1);
+	display_slot[slot].timeout = timeout;
 
 	display_update(false);
 	string_clear(dst);
 
-	string_format(dst, "display setslot: set slot %d on %s tag %s to \"%s\"\n",
-				slot, display_info_entry->name,
-				display_data_entry->slot[slot].tag,
-				display_data_entry->slot[slot].content);
+	string_format(dst, "display-set: set slot %d with tag %s to \"%s\"\n",
+				slot, display_slot[slot].tag,
+				display_slot[slot].content);
 
 	return(app_action_normal);
 }
--- display.h
+++ display.h
@@ -6,32 +6,8 @@
 
 #include <stdint.h>
 
-typedef enum
-{
-	display_slot_amount = 8,
-	display_slot_tag_size = 32,
-	display_slot_content_size = 64
-} display_slot_enum_t;
-
-assert_size(display_slot_enum_t, 4);
-
-typedef struct
-{
-	int		timeout;
-	char	tag[display_slot_tag_size + 1];
-	char	content[display_slot_content_size + 1];
-} display_slot_t;
-
-typedef struct
-{
-	unsigned int	detected:1;
-	uint8_t			current_slot;
-	uint8_t			brightness;
-	display_slot_t	slot[display_slot_amount];
-} display_data_t;
-
-void	display_init(void);
-void	display_periodic(void);
+void display_init(void);
+void display_periodic(void);
 
 app_action_t application_function_display_brightness(const string_t *src, string_t *dst);
 app_action_t application_function_display_dump(const string_t *src, string_t *dst);
