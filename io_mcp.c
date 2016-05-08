61adf1d6d7c4d21bc76a350be53778ccfc4fbf80
Erik Slagter <erik@slagter.name>
2016-05-08 11:18:14 +0200

    mcp: more efficiency by omitting reading pin status before writing.

--- io_mcp.c
+++ io_mcp.c
@@ -13,9 +13,37 @@ typedef struct
 	uint32_t debounce;
 } mcp_data_pin_t;
 
+enum
+{
+	_IODIR = 0x00,
+	_IPOL = 0x02,
+	_GPINTEN = 0x04,
+	_DEFVAL = 0x06,
+	_INTCON = 0x08,
+	_IOCON = 0x0a,
+	_GPPU = 0x0c,
+	_INTF = 0x0e,
+	_INTCAP = 0x10,
+	_GPIO = 0x12,
+	_OLAT = 0x14,
+};
+
+#define IODIR(s)	(_IODIR + s)
+#define	IPOL(s)		(_IPOL + s)
+#define GPINTEN(s)	(_GPINTEN + s)
+#define DEFVAL(s)	(_DEFVAL + s)
+#define INTCON(s)	(_INTCON + s)
+#define IOCON(s)	(_IOCON + s)
+#define GPPU(s)		(_GPPU + s)
+#define INTF(s)		(_INTF + s)
+#define INTCAP(s)	(_INTCAP + s)
+#define GPIO(s)		(_GPIO + s)
+#define OLAT(s)		(_OLAT + s)
+
+static uint8_t pin_output_cache[2];
 static mcp_data_pin_t mcp_data_pin_table[io_mcp_instance_size][16];
 
-irom static io_error_t readregister(string_t *error_message, int address, int reg, int *value)
+irom static io_error_t read_register(string_t *error_message, int address, int reg, int *value)
 {
 	uint8_t i2cbuffer[2];
 	i2c_error_t error;
@@ -43,7 +71,26 @@ irom static io_error_t readregister(string_t *error_message, int address, int re
 	return(io_ok);
 }
 
-irom static io_error_t clearsetregister(string_t *error_message, int address, int reg, int clearmask, int setmask)
+irom static io_error_t write_register(string_t *error_message, int address, int reg, int value)
+{
+	uint8_t i2cbuffer[2];
+	i2c_error_t error;
+
+	i2cbuffer[0] = reg;
+	i2cbuffer[1] = value;
+
+	if((error = i2c_send(address, 2, &i2cbuffer[0])) != i2c_error_ok)
+	{
+		if(error_message)
+			i2c_error_format_string(error_message, error);
+
+		return(io_error);
+	}
+
+	return(io_ok);
+}
+
+irom static io_error_t clear_set_register(string_t *error_message, int address, int reg, int clearmask, int setmask)
 {
 	uint8_t i2cbuffer[2];
 	i2c_error_t error;
@@ -69,10 +116,10 @@ irom static io_error_t clearsetregister(string_t *error_message, int address, in
 	i2cbuffer[1] &= ~clearmask;
 	i2cbuffer[1] |= setmask;
 
-	if(i2c_send(address, 2, i2cbuffer) != i2c_error_ok)
+	if((error = i2c_send(address, 2, &i2cbuffer[0])) != i2c_error_ok)
 	{
 		if(error_message)
-			string_cat(error_message, "can't write to device\n");
+			i2c_error_format_string(error_message, error);
 
 		return(io_error);
 	}
@@ -87,13 +134,13 @@ irom io_error_t io_mcp_init(const struct io_info_entry_T *info)
 	uint8_t i2cbuffer[0x16];
 	mcp_data_pin_t *mcp_pin_data;
 
-	if(i2c_send_1(info->address, 0x00) != i2c_error_ok)
+	if(i2c_send_1(info->address, IODIR(0)) != i2c_error_ok)
 		return(io_error);
 
 	if(i2c_receive(info->address, sizeof(i2cbuffer), i2cbuffer) != i2c_error_ok)
 		return(io_error);
 
-	for(ix = 0x06; ix < 0x0b; ix++)
+	for(ix = DEFVAL(0); ix < 0x0b; ix++)
 		if(i2cbuffer[ix] != 0x00)
 			return(io_error);
 
@@ -112,6 +159,9 @@ irom io_error_t io_mcp_init(const struct io_info_entry_T *info)
 		}
 	}
 
+	pin_output_cache[0] = 0;
+	pin_output_cache[1] = 0;
+
 	return(io_ok);
 }
 
@@ -123,11 +173,11 @@ irom void io_mcp_periodic(int io, const struct io_info_entry_T *info, io_data_en
 	mcp_data_pin_t *mcp_pin_data;
 	io_config_pin_entry_t *pin_config;
 
-	readregister((string_t *)0, info->address, /* INTF */ 0x0e + 0, &intf[0]);
-	readregister((string_t *)0, info->address, /* INTF */ 0x0e + 1, &intf[1]);
+	read_register((string_t *)0, info->address, INTF(0), &intf[0]);
+	read_register((string_t *)0, info->address, INTF(1), &intf[1]);
 
-	readregister((string_t *)0, info->address, /* INTCAP */ 0x10 + 0, &intcap[0]);
-	readregister((string_t *)0, info->address, /* INTCAP */ 0x10 + 1, &intcap[1]);
+	read_register((string_t *)0, info->address, INTCAP(0), &intcap[0]);
+	read_register((string_t *)0, info->address, INTCAP(1), &intcap[1]);
 
 	for(pin = 0; pin < 16; pin++)
 	{
@@ -166,25 +216,25 @@ irom io_error_t io_mcp_init_pin_mode(string_t *error_message, const struct io_in
 	bank = (pin & 0x08) >> 3;
 	bankpin = pin & 0x07;
 
-	if(clearsetregister(error_message, info->address, 0x02 + bank, 1 << bankpin, 0) != io_ok) // polarity inversion = 0
+	if(clear_set_register(error_message, info->address, IPOL(bank), 1 << bankpin, 0) != io_ok) // polarity inversion = 0
 		return(io_error);
 
-	if(clearsetregister(error_message, info->address, 0x04 + bank, 1 << bankpin, 0) != io_ok) // pc int enable = 0
+	if(clear_set_register(error_message, info->address, GPINTEN(bank), 1 << bankpin, 0) != io_ok) // pc int enable = 0
 		return(io_error);
 
-	if(clearsetregister(error_message, info->address, 0x06 + bank, 1 << bankpin, 0) != io_ok) // compare value = 0
+	if(clear_set_register(error_message, info->address, DEFVAL(bank), 1 << bankpin, 0) != io_ok) // compare value = 0
 		return(io_error);
 
-	if(clearsetregister(error_message, info->address, 0x08 + bank, 1 << bankpin, 0) != io_ok) // compare source = 0
+	if(clear_set_register(error_message, info->address, INTCON(bank), 1 << bankpin, 0) != io_ok) // compare source = 0
 		return(io_error);
 
-	if(clearsetregister(error_message, info->address, 0x0c + bank, 1 << bankpin, 0) != io_ok) // pullup = 0
+	if(clear_set_register(error_message, info->address, GPPU(bank), 1 << bankpin, 0) != io_ok) // pullup = 0
 		return(io_error);
 
-	if(clearsetregister(error_message, info->address, 0x12 + bank, 1 << bankpin, 0) != io_ok) // gpio = 0
+	if(clear_set_register(error_message, info->address, GPIO(bank), 1 << bankpin, 0) != io_ok) // gpio = 0
 		return(io_error);
 
-	if(clearsetregister(error_message, info->address, 0x14 + bank, 1 << bankpin, 0) != io_ok) // latch = 0
+	if(clear_set_register(error_message, info->address, OLAT(bank), 1 << bankpin, 0) != io_ok) // latch = 0
 		return(io_error);
 
 	switch(pin_config->llmode)
@@ -197,13 +247,13 @@ irom io_error_t io_mcp_init_pin_mode(string_t *error_message, const struct io_in
 		case(io_pin_ll_input_digital):
 		case(io_pin_ll_counter):
 		{
-			if(clearsetregister(error_message, info->address, 0x00 + bank, 0, 1 << bankpin) != io_ok) // direction = 1
+			if(clear_set_register(error_message, info->address, IODIR(bank), 0, 1 << bankpin) != io_ok) // direction = 1
 				return(io_error);
 
-			if(pin_config->flags.pullup && (clearsetregister(error_message, info->address, 0x0c + bank, 0, 1 << bankpin) != io_ok))
+			if(pin_config->flags.pullup && (clear_set_register(error_message, info->address, GPPU(bank), 0, 1 << bankpin) != io_ok))
 				return(io_error);
 
-			if((pin_config->llmode == io_pin_ll_counter) && (clearsetregister(error_message, info->address, 0x04 + bank, 0, 1 << bankpin) != io_ok)) // pc int enable = 1
+			if((pin_config->llmode == io_pin_ll_counter) && (clear_set_register(error_message, info->address, GPINTEN(bank), 0, 1 << bankpin) != io_ok)) // pc int enable = 1
 				return(io_error);
 
 			break;
@@ -211,7 +261,7 @@ irom io_error_t io_mcp_init_pin_mode(string_t *error_message, const struct io_in
 
 		case(io_pin_ll_output_digital):
 		{
-			if(clearsetregister(error_message, info->address, 0x00 + bank, 1 << bankpin, 0) != io_ok) // direction = 0
+			if(clear_set_register(error_message, info->address, IODIR(bank), 1 << bankpin, 0) != io_ok) // direction = 0
 				return(io_error);
 
 			break;
@@ -231,7 +281,8 @@ irom io_error_t io_mcp_init_pin_mode(string_t *error_message, const struct io_in
 
 irom io_error_t io_mcp_get_pin_info(string_t *dst, const struct io_info_entry_T *info, io_data_pin_entry_t *pin_data, const io_config_pin_entry_t *pin_config, int pin)
 {
-	int bank, bankpin, tv, value;
+	int bank, bankpin, tv;
+	int io, olat, cached;
 	mcp_data_pin_t *mcp_pin_data;
 
 	bank = (pin & 0x08) >> 3;
@@ -302,7 +353,7 @@ irom io_error_t io_mcp_read_pin(string_t *error_message, const struct io_info_en
 		case(io_pin_ll_input_digital):
 		case(io_pin_ll_output_digital):
 		{
-			if(readregister(error_message, info->address, 0x12 + bank, &tv) != io_ok)
+			if(read_register(error_message, info->address, GPIO(bank), &tv) != io_ok)
 				return(io_error);
 
 			*value = !!(tv & (1 << bankpin));
@@ -331,7 +382,7 @@ irom io_error_t io_mcp_read_pin(string_t *error_message, const struct io_info_en
 
 irom io_error_t io_mcp_write_pin(string_t *error_message, const struct io_info_entry_T *info, io_data_pin_entry_t *pin_data, const io_config_pin_entry_t *pin_config, int pin, int value)
 {
-	int bank, bankpin, clearmask, setmask;
+	int bank, bankpin;
 	mcp_data_pin_t *mcp_pin_data;
 
 	bank = (pin & 0x08) >> 3;
@@ -340,21 +391,15 @@ irom io_error_t io_mcp_write_pin(string_t *error_message, const struct io_info_e
 	mcp_pin_data = &mcp_data_pin_table[info->instance][pin];
 
 	if(value)
-	{
-		clearmask = 0x00;
-		setmask = 1 << bankpin;
-	}
+		pin_output_cache[bank] |= 1 << bankpin;
 	else
-	{
-		clearmask = 1 << bankpin;
-		setmask = 0x00;
-	}
+		pin_output_cache[bank] &= ~(1 << bankpin);
 
 	switch(pin_config->llmode)
 	{
 		case(io_pin_ll_output_digital):
 		{
-			if(clearsetregister(error_message, info->address, 0x12 + bank, clearmask, setmask) != io_ok)
+			if(write_register(error_message, info->address, GPIO(bank), pin_output_cache[bank]) != io_ok)
 				return(io_error);
 
 			break;
