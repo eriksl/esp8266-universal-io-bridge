7f01dff335b356db7ae0873e7305379c1ba58a4a
Erik Slagter <erik@openpli.org>
2017-09-02 19:51:23 +0200

    Improve http error handling.

--- http.c
+++ http.c
@@ -12,6 +12,11 @@ roflash static const char http_header_pre[] =
 	"HTTP/1.0 "
 };
 
+roflash static const char http_eol[] =
+{
+	"\r\n"
+};
+
 roflash static const char http_header_ok[] =
 {
 	"200 OK\r\n"
@@ -24,7 +29,6 @@ roflash static const char http_header_ok[] =
 roflash static const char http_header_error[] =
 {
 	"Content-Type: text/html; charset=UTF-8\r\n"
-	"Content-Length: 0\r\n"
 	"Connection: close\r\n"
 	"\r\n"
 };
@@ -46,6 +50,27 @@ roflash static const char html_footer[] =
 	"</html>\n"
 };
 
+irom static app_action_t http_error(string_t *dst, const char *error_string, const char *info)
+{
+	string_cat_ptr(dst, http_header_pre);
+	string_cat_strptr(dst, error_string);
+	string_cat_ptr(dst, http_eol);
+	string_cat_ptr(dst, http_header_error);
+	string_cat_ptr(dst, html_header);
+	string_cat_strptr(dst, error_string);
+
+	if(info)
+	{
+		string_cat(dst, ": ");
+		string_cat_strptr(dst, info);
+	}
+
+	string_cat_ptr(dst, "\n");
+	string_cat_ptr(dst, html_footer);
+
+	return(app_action_error);
+}
+
 irom app_action_t application_function_http_get(const string_t *src, string_t *dst)
 {
 	string_new(static, location, 32);
@@ -56,24 +81,14 @@ irom app_action_t application_function_http_get(const string_t *src, string_t *d
 	string_clear(&location);
 
 	if((parse_string(1, src, &location)) != parse_ok)
-	{
-		string_cat_ptr(dst, http_header_pre);
-		string_cat(dst, "400 Bad Request\r\n");
-		string_cat_ptr(dst, http_header_error);
-		return(app_action_error);
-	}
+		return(http_error(dst, "400 Bad Request", 0));
 
 	for(handler = &handlers[0]; handler->location && handler->handler; handler++)
 		if(string_match(&location, handler->location))
 			break;
 
 	if(!handler->location || !handler->handler)
-	{
-		string_cat_ptr(dst, http_header_pre);
-		string_format(dst, "404 Not Found: %s\r\n", string_to_const_ptr(&location));
-		string_cat_ptr(dst, http_header_error);
-		return(app_action_error);
-	}
+		return(http_error(dst, "404 Not Found", string_to_const_ptr(&location)));
 
 	string_clear(dst);
 	string_cat_ptr(dst, http_header_pre);
@@ -85,20 +100,10 @@ irom app_action_t application_function_http_get(const string_t *src, string_t *d
 		string_cat_ptr(dst, html_footer);
 
 		if((length = string_length(dst) - (sizeof(http_header_pre) - 1) - (sizeof(http_header_ok) - 1)) <= 0)
-		{
-			string_cat_ptr(dst, http_header_pre);
-			string_copy(dst, "500 Internal Server Error\r\n");
-			string_cat_ptr(dst, http_header_error);
-			return(app_action_error);
-		}
+			return(http_error(dst, "500 Internal Server Error", 0));
 
 		if((ix = string_find(dst, 0, '@')) <= 0)
-		{
-			string_cat_ptr(dst, http_header_pre);
-			string_copy(dst, "501 Not Implemented\r\n");
-			string_cat_ptr(dst, http_header_error);
-			return(app_action_error);
-		}
+			return(http_error(dst, "501 Not Implemented", 0));
 
 		string_replace(dst, ix + 0, (length / 1000) + '0');
 		length %= 1000;
