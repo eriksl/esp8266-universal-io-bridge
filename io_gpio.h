05ec02cce807c92d5d6a70650578e4ef61df2035
Erik Slagter <erik@openpli.org>
2018-07-02 19:01:48 +0200

    gpio.h: add -O3 (attr_speed) to inline I/O functions.
    This might make them faster, but will probably do nothing.

--- io_gpio.h
+++ io_gpio.h
@@ -20,38 +20,38 @@ app_action_t application_function_pwm_period(const string_t *src, string_t *dst)
 
 // generic
 
-always_inline static uint32_t gpio_pin_addr(int pin)
+always_inline attr_speed static uint32_t gpio_pin_addr(int pin)
 {
 	return(GPIO_PIN0_ADDRESS + (pin << 2));
 }
 
 // read / write registers
 
-always_inline static uint32_t read_peri_reg(uint32_t addr)
+always_inline attr_speed static uint32_t read_peri_reg(uint32_t addr)
 {
 	volatile uint32_t *ptr = (volatile uint32_t *)addr;
 
 	return(*ptr);
 }
 
-always_inline static void write_peri_reg(volatile uint32_t addr, uint32_t value)
+always_inline attr_speed static void write_peri_reg(volatile uint32_t addr, uint32_t value)
 {
 	volatile uint32_t *ptr = (volatile uint32_t *)addr;
 
 	*ptr = value;
 }
 
-always_inline static void clear_peri_reg_mask(volatile uint32_t addr, uint32_t mask)
+always_inline attr_speed static void clear_peri_reg_mask(volatile uint32_t addr, uint32_t mask)
 {
 	write_peri_reg(addr, read_peri_reg(addr) & ~mask);
 }
 
-always_inline static void set_peri_reg_mask(volatile uint32_t addr, uint32_t mask)
+always_inline attr_speed static void set_peri_reg_mask(volatile uint32_t addr, uint32_t mask)
 {
 	write_peri_reg(addr, read_peri_reg(addr) | mask);
 }
 
-always_inline static void clear_set_peri_reg_mask(uint32_t addr, uint32_t clearmask, uint32_t setmask)
+always_inline attr_speed static void clear_set_peri_reg_mask(uint32_t addr, uint32_t clearmask, uint32_t setmask)
 {
 	clear_peri_reg_mask(addr, clearmask);
 	set_peri_reg_mask(addr, setmask);
@@ -59,41 +59,41 @@ always_inline static void clear_set_peri_reg_mask(uint32_t addr, uint32_t clearm
 
 // read write GPIO registers
 
-always_inline static uint32_t gpio_reg_read(int reg)
+always_inline attr_speed static uint32_t gpio_reg_read(int reg)
 {
 	return(read_peri_reg(PERIPHS_GPIO_BASEADDR + reg));
 }
 
-always_inline static void gpio_reg_write(int reg, uint32_t value)
+always_inline attr_speed static void gpio_reg_write(int reg, uint32_t value)
 {
 	write_peri_reg(PERIPHS_GPIO_BASEADDR + reg, value);
 }
 
 // read input
 
-always_inline static uint32_t gpio_get_all(void)
+always_inline attr_speed static uint32_t gpio_get_all(void)
 {
 	return(gpio_reg_read(GPIO_IN_ADDRESS));
 }
 
-always_inline static int gpio_get(int io)
+always_inline attr_speed static int gpio_get(int io)
 {
 	return((gpio_get_all() & (1 << io)) ? 1 : 0);
 }
 
 // set output low / high
 
-always_inline static void gpio_clear_mask(uint32_t mask)
+always_inline attr_speed static void gpio_clear_mask(uint32_t mask)
 {
 	gpio_reg_write(GPIO_OUT_W1TC_ADDRESS, mask);
 }
 
-always_inline static void gpio_set_mask(uint32_t mask)
+always_inline attr_speed static void gpio_set_mask(uint32_t mask)
 {
 	gpio_reg_write(GPIO_OUT_W1TS_ADDRESS, mask);
 }
 
-always_inline static void gpio_set(int io, int onoff)
+always_inline attr_speed static void gpio_set(int io, int onoff)
 {
 	if(onoff)
 		gpio_set_mask(1 << io);
