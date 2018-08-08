1fc54d0602337f6dd2c3ef9e442f76b31667f258
Erik Slagter <erik@openpli.org>
2018-08-08 20:36:49 +0200

    queue: move most used functions from iram to inline.
    Also remove lf() member function that wasn't used anymore.

--- queue.c
+++ queue.c
@@ -1,56 +1,9 @@
 #include "queue.h"
 
-#include "util.h"
-
 irom void queue_new(queue_t *queue, int size, char *buffer)
 {
 	queue->data = buffer;
 	queue->size = size;
 	queue->in = 0;
 	queue->out = 0;
-	queue->lf = 0;
-}
-
-attr_speed iram attr_pure char queue_empty(const queue_t *queue)
-{
-	return(queue->in == queue->out);
-}
-
-attr_speed iram attr_pure char queue_full(const queue_t *queue)
-{
-	return(((queue->in + 1) % queue->size) == queue->out);
-}
-
-attr_speed iram attr_pure int queue_lf(const queue_t *queue)
-{
-	return(queue->lf);
-}
-
-attr_speed iram void queue_flush(queue_t *queue)
-{
-	queue->in = 0;
-	queue->out = 0;
-	queue->lf = 0;
-}
-
-attr_speed iram void queue_push(queue_t *queue, char data)
-{
-	if(data == '\n')
-		queue->lf++;
-
-	queue->data[queue->in] = data;
-	queue->in = (queue->in + 1) % queue->size;
-}
-
-attr_speed iram char queue_pop(queue_t *queue)
-{
-	char data;
-
-	data = queue->data[queue->out];
-	queue->out = (queue->out + 1) % queue->size;
-
-	if(data == '\n')
-		queue->lf--;
-
-	return(data);
 }
--- queue.h
+++ queue.h
@@ -3,21 +3,48 @@
 
 #include <stdint.h>
 
+#include "util.h"
+
 typedef struct
 {
 	char *data;
 	int size;
 	int in;
 	int out;
-	int lf;
 } queue_t;
 
 void queue_new(queue_t *queue, int size, char *buffer);
-char queue_empty(const queue_t *queue);
-char queue_full(const queue_t *queue);
-int queue_lf(const queue_t *queue);
-void queue_flush(queue_t *queue);
-void queue_push(queue_t *queue, char data);
-char queue_pop(queue_t *queue);
+
+always_inline static attr_pure char queue_empty(const queue_t *queue)
+{
+	return(queue->in == queue->out);
+}
+
+always_inline static attr_pure char queue_full(const queue_t *queue)
+{
+	return(((queue->in + 1) % queue->size) == queue->out);
+}
+
+always_inline static void queue_flush(queue_t *queue)
+{
+	queue->in = 0;
+	queue->out = 0;
+}
+
+always_inline static void queue_push(queue_t *queue, char data)
+{
+	queue->data[queue->in] = data;
+	queue->in = (queue->in + 1) % queue->size;
+}
+
+always_inline static char queue_pop(queue_t *queue)
+{
+	char data;
+
+	data = queue->data[queue->out];
+	queue->out = (queue->out + 1) % queue->size;
+
+	return(data);
+}
 
 #endif
