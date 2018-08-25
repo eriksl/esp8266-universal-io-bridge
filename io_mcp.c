f84cb7874e249af979f9f5adb21accde8cd1f5d8
Erik Slagter <erik@openpli.org>
2018-08-25 10:01:24 +0200

    io_mcp: pin_write may be called from timer, move it to IRAM.

--- io_mcp.c
+++ io_mcp.c
@@ -360,7 +360,7 @@ iram io_error_t io_mcp_read_pin(string_t *error_message, const struct io_info_en
 	return(io_ok);
 }
 
-irom io_error_t io_mcp_write_pin(string_t *error_message, const struct io_info_entry_T *info, io_data_pin_entry_t *pin_data, const io_config_pin_entry_t *pin_config, int pin, uint32_t value)
+iram io_error_t io_mcp_write_pin(string_t *error_message, const struct io_info_entry_T *info, io_data_pin_entry_t *pin_data, const io_config_pin_entry_t *pin_config, int pin, uint32_t value)
 {
 	int bank, bankpin;
 	mcp_data_pin_t *mcp_pin_data;
