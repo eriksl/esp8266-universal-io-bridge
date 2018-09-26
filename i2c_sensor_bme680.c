3a7216d58bf094056c3ad565bc34ed87d9fd3104
Erik Slagter <erik@openpli.org>
2018-09-26 20:40:54 +0200

    bme680: the ohmic resistance baseline seems to be actually 200000.
    It needs to be tweaked using sensor calibration on a per-sensor and
    per-site basis.

--- i2c_sensor_bme680.c
+++ i2c_sensor_bme680.c
@@ -1896,7 +1896,7 @@ irom static i2c_error_t sensor_bme680_read(void)
 
 		if((data.status & BME680_GASM_VALID_MSK) && (data.status & BME680_HEAT_STAB_MSK))
 		{
-			airquality.cooked = airquality.raw * 100 / 35000;
+			airquality.cooked = airquality.raw * 100 / 200000;
 
 			if(airquality.cooked < 0)
 				airquality.cooked = 0;
