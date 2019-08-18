09baaf911592eab135a7956823f4ca231b84679b
Erik Slagter <erik@openpli.org>
2019-08-18 16:30:52 +0200

    display: add 6x8 font for pure graphic displays.

---
+++ display_font_6x8.h
@@ -0,0 +1,13 @@
+#ifndef _display_font_6x8_h_
+#define _display_font_6x8_h_
+
+#include <stdint.h>
+
+enum
+{
+	display_font_6x8_size = 256,
+};
+
+extern const uint8_t display_font_6x8[display_font_6x8_size * 8];
+
+#endif
