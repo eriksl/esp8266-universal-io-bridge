6b886d04eb3c4a664e9abe1d03625b9369a77ed1
Erik Slagter <erik@openpli.org>
2020-03-15 17:46:46 +0100

    resetserial: add extra modes for testing.

--- resetserial.c
+++ resetserial.c
@@ -10,12 +10,21 @@ int main(int argc, char **argv)
 	int fd, flags;
 	int delay = 10000;
 	struct termios termios;
+	unsigned int mode = 0;
 
-	if(argc != 2)
+	if((argc != 2) && (argc != 3))
 	{
-		fprintf(stderr, "usage: resetserial <device>\n");
+		fprintf(stderr, "usage: resetserial <device> [mode]\n");
+		fprintf(stderr, "       mode = 0: default\n");
+		fprintf(stderr, "       mode = 1: reset RTS+DTR\n");
+		fprintf(stderr, "       mode = 2: reset DTR, set RTS\n");
+		fprintf(stderr, "       mode = 3: reset RTS, set DTR\n");
+		fprintf(stderr, "       mode = 4: set RTS+DTR\n");
 		exit(1);
 	}
+
+	if(argc == 3)
+		mode = strtoul(argv[2], (char **)0, 0);
 
 	if((fd = open(argv[1], O_RDONLY | O_NOCTTY, 0)) < 0)
 	{
@@ -46,48 +55,118 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 
-	// reset
-
-	flags &= ~TIOCM_RTS;
-
-	if(ioctl(fd, TIOCMSET, &flags))
+	switch(mode)
 	{
-		perror("ioctl 2\n");
-		exit(1);
-	}
+		case(0):
+		{
 
-	usleep(delay);
+			// set reset
 
-	// gpio0
+			flags &= ~TIOCM_RTS;
 
-	flags &= ~TIOCM_DTR;
+			if(ioctl(fd, TIOCMSET, &flags))
+			{
+				perror("ioctl 2\n");
+				exit(1);
+			}
 
-	if(ioctl(fd, TIOCMSET, &flags))
-	{
-		perror("ioctl 3\n");
-		exit(1);
-	}
+			usleep(delay);
 
-	usleep(delay);
+			// set gpio0
 
-	// !reset
+			if(mode)
+				flags |= TIOCM_DTR;
+			else
+				flags &= ~TIOCM_DTR;
 
-	flags |= TIOCM_RTS;
+			if(ioctl(fd, TIOCMSET, &flags))
+			{
+				perror("ioctl 3\n");
+				exit(1);
+			}
 
-	if(ioctl(fd, TIOCMSET, &flags))
-	{
-		perror("ioctl 4\n");
-		exit(1);
-	}
+			usleep(delay);
 
-	usleep(delay);
+			// release reset
 
-	flags |= TIOCM_DTR;
+			flags |= TIOCM_RTS;
 
-	if(ioctl(fd, TIOCMSET, &flags))
-	{
-		perror("ioctl 5\n");
-		exit(1);
+			if(ioctl(fd, TIOCMSET, &flags))
+			{
+				perror("ioctl 4\n");
+				exit(1);
+			}
+
+			usleep(delay);
+
+			// release gpio0
+
+			flags |= TIOCM_DTR;
+
+			if(ioctl(fd, TIOCMSET, &flags))
+			{
+				perror("ioctl 5\n");
+				exit(1);
+			}
+
+			break;
+		}
+
+		case(1):
+		{
+			flags &= ~TIOCM_RTS;
+			flags &= ~TIOCM_DTR;
+
+			if(ioctl(fd, TIOCMSET, &flags))
+			{
+				perror("ioctl 12\n");
+				exit(1);
+			}
+
+			break;
+		}
+
+		case(2):
+		{
+			flags |= TIOCM_RTS;
+			flags &= ~TIOCM_DTR;
+
+			if(ioctl(fd, TIOCMSET, &flags))
+			{
+				perror("ioctl 22\n");
+				exit(1);
+			}
+
+			break;
+		}
+
+		case(3):
+		{
+			flags &= ~TIOCM_RTS;
+			flags |= TIOCM_DTR;
+
+			if(ioctl(fd, TIOCMSET, &flags))
+			{
+				perror("ioctl 32\n");
+				exit(1);
+			}
+
+			break;
+		}
+
+		case(4):
+		{
+			flags |= TIOCM_RTS;
+			flags |= TIOCM_DTR;
+
+			if(ioctl(fd, TIOCMSET, &flags))
+			{
+				perror("ioctl 42\n");
+				exit(1);
+			}
+
+			break;
+		}
 	}
 
 	exit(0);
