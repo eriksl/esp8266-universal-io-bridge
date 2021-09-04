9a887e9533e7a816130bbdcef23ae7a5555a52d4
Erik Slagter <erik@openpli.org>
2021-09-04 11:13:49 +0200

    lwiptoptions: increase amount of udp pcb buffers.
    Otherwise it would crash if the bridge port would be enabled.

--- lwipopts.h
+++ lwipopts.h
@@ -2,7 +2,7 @@
 
 #define MEM_SIZE					(6 * 1024)
 #define MEMP_NUM_PBUF				16
-#define MEMP_NUM_UDP_PCB			4
+#define MEMP_NUM_UDP_PCB			8 // do not decrease!
 #define MEMP_NUM_TCP_PCB			2
 #define MEMP_NUM_TCP_PCB_LISTEN		2
 #define MEMP_NUM_TCP_SEG			TCP_SND_QUEUELEN
