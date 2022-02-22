1a2403c6b6599cc0f965e5e7d276227b6715fdc6
Erik Slagter <erik@openpli.org>
2022-02-22 20:51:06 +0100

    display_ssd1306: move some generic code for 6x8 font from ssd1306 to font6x8.

--- display_font_6x8.h
+++ display_font_6x8.h
@@ -5,9 +5,24 @@
 
 enum
 {
+	display_font_width = 6,
+	display_font_height = 8,
 	display_font_6x8_size = 256,
 };
 
 extern const uint8_t display_font_6x8[display_font_6x8_size * 8];
+
+enum
+{
+	unicode_map_6x8_eof = 0xffffffff,
+};
+
+typedef struct
+{
+	unsigned int unicode;
+	unsigned int internal;
+} unicode_map_6x8_t;
+
+extern const unicode_map_6x8_t unicode_map_6x8[];
 
 #endif
